cff3f6ba28d28d0a122498c3b60b4708bd4fa043
KingYes <KingYes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-19 21:08:19 +0000

    first commit of pojo-lightbox
    git-svn-id: https://plugins.svn.wordpress.org/pojo-lightbox/trunk@968568 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/strings.php
@@ -0,0 +1,4 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+__( 'This plugin used to add the lightbox (overlay) effect to all images on your WordPress site with Pojo Framework', 'pojo-lightbox' );
